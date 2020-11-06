07edee0514cb40f71ae7abdcea35ef0d56c07c2f
maitreykamble <71888113+maitreykamble@users.noreply.github.com>
2020-11-06 15:51:41 +0530

    Add files via upload

---
+++ block2.js
@@ -0,0 +1,36 @@
+class Block2 {
+    constructor(x, y, width, height){
+      var options = {
+        restitution:0.8,
+        friction:1.0,
+        density:1.0,
+        isStatic: false
+      
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.visibility = 255;
+    World.add(world, this.body);
+  }
+  display(){
+    if (this.body.speed < 3) {
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("pink");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+    else {
+      World.remove(world, this.body);
+      push();
+      this.visibility = this.visibility - 5;
+      tint(255, this.visibility);
+      pop();
+    }
+
+    
+  
+  }
+}
+    
+    
---
+++ block3.js
@@ -0,0 +1,39 @@
+class Block3 {
+    constructor(x, y, width, height){
+      var options = {
+        restitution:0.8,
+        friction:1.0,
+        density:1.0,
+        isStatic: false
+      
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.visibility = 255;
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+   
+  }
+  display(){
+       
+    if (this.body.speed < 5) {
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("lightGreen");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+    else {
+      World.remove(world, this.body);
+      push();
+      this.visibility = this.visibility - 5;
+      tint(255, this.visibility);
+      pop();
+    }
+
+    
+  
+    
+  }
+}
+
+  
